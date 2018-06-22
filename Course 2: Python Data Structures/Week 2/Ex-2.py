38ca315a95df350868a731d4dc835a3cce3c5a7d
Shubhi Srivastava <34751123+shubhi1998@users.noreply.github.com>
2018-06-22 22:52:03 +0530

    Update Ex-2.py

--- Course 2: Python Data Structures/Week 2/Ex-2.py
+++ Course 2: Python Data Structures/Week 2/Ex-2.py
@@ -1,9 +1,33 @@
-fhand = open('mbox-short.txt')
-tot = 0.0
-count = 0
+Write a program to prompt for a file name, and then read through the
+file and look for lines of the form:
+X-DSPAM-Confidence:0.8475
+When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart
+the line to extract the floating-point number on the line. Count these lines and
+then compute the total of the spam confidence values from these lines. then compute the total of the spam confidence values from these lines. 
+
+
+
+CODE
+filename = input("Enter the file name")
+try:
+    fhand = open(filename)
+except:
+    print("This file can't be printed")
+    quit()
+count =0
+sum =0.0
+
 for line in fhand:
+    line = line.rstrip()
+
+
+
+
     if line.startswith("X-DSPAM-Confidence:"):
-        words = line.split()
-        tot = tot + float(words[1])
-        count = count + 1
-print("Average spam confidence:",tot/count)
+
+        atpos = line.find(":")
+        spos = line[atpos+1:]
+        sum = sum + float(spos)
+        count=count+1
+avg=sum/count
+print"Average of confidence values: ",avg
