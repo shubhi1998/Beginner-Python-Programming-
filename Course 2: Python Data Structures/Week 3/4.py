1dd68125dc733310d8d23547dc277ec776929709
Shubhi Srivastava <34751123+shubhi1998@users.noreply.github.com>
2018-07-21 21:36:22 +0530

    Update 4.py

--- Course 2: Python Data Structures/Week 3/4.py
+++ Course 2: Python Data Structures/Week 3/4.py
@@ -1,12 +1,18 @@
-fhand = open("romeo.txt")
-word = list()
+name = input("Enter the file name")
+try:
+    fhand = open(name)
+except:
+    print("This file can't be opened")
+    exit()
+lst= list()
 for line in fhand:
-    line = line.strip()
-    words = line.split()
-    word.append(words)
-count = list()
-for w in word:
-    if w not in count:
-        count.append(w)
-print("All the words:",word)
-print("After removing the duplicates:",count)
+    line = line.rstrip()
+    words= line.split()
+    for word in words:
+        if word not in lst:
+            lst.append(word)
+        else:
+            continue
+lst.sort()
+print(lst)
+
