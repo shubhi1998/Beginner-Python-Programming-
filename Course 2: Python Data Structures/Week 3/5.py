741dbd9c66a88a797a347ae7fe4fffdca2c15f9a
Shubhi Srivastava <34751123+shubhi1998@users.noreply.github.com>
2018-07-21 21:46:21 +0530

    Update 5.py

--- Course 2: Python Data Structures/Week 3/5.py
+++ Course 2: Python Data Structures/Week 3/5.py
@@ -1,12 +1,15 @@
-fhand = open('mbox-short.txt')
-counter = 0
-
-
-for line in fhand :
-    line = line.strip()
-    if not line.startswith('From '): continue
-    words = line.split()
-    print( words[1])
-    counter +=1
-
-print ("There were", counter, "lines in the file with From as the first word")
+filename = input("Enter the file name")
+try:
+    fhand = open(filename)
+except:
+    print("This file can't be opened")
+    quit()
+count =0
+for line in fhand:
+    line in line.rstrip()
+    if not line.startswith("From"):
+        continue
+    words =line.split()
+    print(words[1])
+    count = count + 1
+print "There were",count,"line in the file with From as the first word"
